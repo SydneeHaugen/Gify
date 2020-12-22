609015b32d8d76037763a4d46ee0c94e23c0968e
SydneeHaugen <sydhaugen@gmail.com>
2020-12-21 17:35:38 -0800

    updated

--- gify.py
+++ gify.py
@@ -4,11 +4,7 @@ import discord
 import asyncio
 import requests
 import os
-import json
-import random
-import time
-import datetime
-import math
+
 
 
 from discord.ext import commands
@@ -19,7 +15,7 @@ load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='#')
 
 
 
@@ -35,21 +31,10 @@ async def on_ready():
 async def on_message(message):
 
     #jarvis says a random quotes if you speak its name (lowercase only)
-    if 'jarvis' in message.content.lower():
+    if 'gify' in message.content.lower():
 
-        with open('quotes.json', 'r') as quotes:
-            jarvisQuotes = json.load(quotes)
+        await message.channel.send("Hi :)")
 
-        await message.channel.send(random.choice(jarvisQuotes))
-
-    # gifs outputed depending on prompt word 
-    elif 'noice' in message.content.lower() and not message.author.bot:
-        noiceGIF = 'https://tenor.com/view/brooklyn99-noice-jake-peralta-andy-samberg-nice-gif-14234819'
-        await message.channel.send(noiceGIF)
-
-    elif 'what' in message.content.lower() and not message.author.bot:
-        whaaatGIF = 'https://gph.is/g/a9Y71PB'
-        await message.channel.send(whaaatGIF)
 
     await bot.process_commands(message)
 
@@ -61,6 +46,13 @@ async def hello(context):
     msg = 'Hello ' + context.author.mention
 
     await context.send(msg)
+
+# simple example that prints out hello
+@bot.command(name='noice', help='Jarvis says hello.')
+async def noice(context):
+
+    noiceGIF = 'https://tenor.com/view/brooklyn99-noice-jake-peralta-andy-samberg-nice-gif-14234819'
+    await message.channel.send(noiceGIF)
 
 
 
